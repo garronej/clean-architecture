0aed122c4f14744f48e526c4ee334b247f0f53ff
garronej <joseph.garrone.gj@gmail.com>
2021-12-20 07:11:35 +0100

    Add missing index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { usecasesToSelectors } from "./usecasesToSelectors";
 export { usecasesToReducer } from "./usecasesToReducer";
 export { usecasesToAutoDispatchThunks } from "./usecasesToAutoDispatchThunks";
+import { usecasesToPureFunctions } from "./usecasesToPureFunctions";
