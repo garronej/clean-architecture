06eec614e0a61b7cd25db27a2df07b5fabedcd4b
garronej <joseph.garrone.gj@gmail.com>
2021-12-20 07:21:21 +0100

    fix previous commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { usecasesToSelectors } from "./usecasesToSelectors";
 export { usecasesToReducer } from "./usecasesToReducer";
 export { usecasesToAutoDispatchThunks } from "./usecasesToAutoDispatchThunks";
-import { usecasesToPureFunctions } from "./usecasesToPureFunctions";
+export { usecasesToPureFunctions } from "./usecasesToPureFunctions";
