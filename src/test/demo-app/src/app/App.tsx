41780ec1dd7a40b4dad49d47fd4a658f3f2fe57b
garronej <joseph.garrone.gj@gmail.com>
2021-12-20 06:47:22 +0100

    Initial release (changelog ignore)

--- src/test/demo-app/src/app/App.tsx
+++ src/test/demo-app/src/app/App.tsx
@@ -1,21 +1,14 @@
-import { memo, useEffect } from "react";
-import { selectors, useThunks, useSelector } from "app/libApi";
+import { useThunks, useSelector, selectors } from "app/libApi";
 
-export const App = memo(() => {
-    const counter = useSelector(state => state.usecase2.counter2);
-    const { isReady } = useSelector(selectors.usecase2.isReady);
-    const { isReadyBig } = useSelector(selectors.usecase2.isReadyBig);
-    const { usecase1Thunks, usecase2Thunks } = useThunks();
+export function App() {
 
-    useEffect(() => {
-        usecase1Thunks.thunk1({ "pX": "" }).then(v => {
-            /*...*/
-        });
-    }, [usecase1Thunks]);
+    const { usecase1Thunks } = useThunks();
+
+    const { isReady } = useSelector(selectors.usecase1.isReady);
 
     return (
-        <button onClick={() => usecase2Thunks.thunkX({ "pX": "ok" })}>
-            Hello World {counter} {isReady} {isReadyBig}
+        <button onClick={() => usecase1Thunks.thunk1({ pX: "" })}>
+            Hello World {isReady}
         </button>
     );
-});
+};
