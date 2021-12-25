3368623ddf51a56841276d9566e499c9bda199b8
garronej <joseph.garrone.gj@gmail.com>
2021-12-25 09:05:04 +0100

    Expand evtAction type for better experience in vscode type preview

--- src/middlewareEvtAction.ts
+++ src/middlewareEvtAction.ts
@@ -10,6 +10,9 @@ import { exclude } from "tsafe/exclude";
 import { typeGuard } from "tsafe/typeGuard";
 import { symToStr } from "tsafe/symToStr";
 
+/*
+type RecordToUnion<O> = O[keyof O];
+
 type CaseReducerToEvent<T extends Record<string, ActionCreator<any>>> = RecordToUnion<{
     [K in keyof T]: { actionName: K } & (T[K] extends ActionCreatorWithoutPayload<any>
         ? {}
@@ -17,8 +20,6 @@ type CaseReducerToEvent<T extends Record<string, ActionCreator<any>>> = RecordTo
               payload: T[K] extends ActionCreatorWithPayload<infer U> ? U : never;
           });
 }>;
-
-type RecordToUnion<O> = O[keyof O];
 
 type UsecaseToEvent<
     Usecase extends { name: string } & (
@@ -33,6 +34,7 @@ type UsecaseToEvent<
         ? { sliceName: Key } & CaseReducerToEvent<Usecase["actions"]>
         : never;
 }>;
+*/
 
 export function createMiddlewareEvtActionFactory<
     Usecase extends { name: string } & (
@@ -41,7 +43,92 @@ export function createMiddlewareEvtActionFactory<
     ),
 >(usecases: readonly Usecase[]) {
     function createMiddlewareEvtAction(): {
-        evtAction: NonPostableEvt<UsecaseToEvent<Usecase>>;
+        /** NOTE: We use an expended version of the type so that,
+         * when the cursor hovers evtAction we get an explicit type instead of
+         * a composition of union:
+         * This: https://user-images.githubusercontent.com/6702424/147380322-bf2fa468-0a1c-4961-a7d8-16eaa14b6c4e.png
+         * Instead of this: https://user-images.githubusercontent.com/6702424/147380353-9956a98a-9f9c-4811-a8b4-16cd1e4e76ca.png
+         * Don't try, however, to work edit the expanded version. Start from the factorized
+         * form by uncommenting the following line and the helper types it leverages.
+         */
+        //evtAction: NonPostableEvt<UsecaseToEvent<Usecase>>;
+        evtAction: NonPostableEvt<
+            {
+                [Key in Extract<Usecase, { actions: unknown }>["name"]]: Usecase extends Extract<
+                    Usecase,
+                    { name: Key; actions: unknown }
+                >
+                    ? {
+                          [K in keyof Usecase["actions"]]: Usecase["actions"][K] extends ActionCreatorWithoutPayload<any>
+                              ? {
+                                    sliceName: Key;
+                                    actionName: K;
+                                }
+                              : {
+                                    sliceName: Key;
+                                    actionName: K;
+                                    payload: Usecase["actions"][K] extends ActionCreatorWithPayload<
+                                        infer U
+                                    >
+                                        ? U
+                                        : never;
+                                };
+                      }[keyof {
+                          [K in keyof Usecase["actions"]]: Usecase["actions"][K] extends ActionCreatorWithoutPayload<any>
+                              ? {
+                                    sliceName: Key;
+                                    actionName: K;
+                                }
+                              : {
+                                    sliceName: Key;
+                                    actionName: K;
+                                    payload: Usecase["actions"][K] extends ActionCreatorWithPayload<
+                                        infer U
+                                    >
+                                        ? U
+                                        : never;
+                                };
+                      }]
+                    : never;
+            }[keyof {
+                [Key in Extract<Usecase, { actions: unknown }>["name"]]: Usecase extends Extract<
+                    Usecase,
+                    { name: Key; actions: unknown }
+                >
+                    ? {
+                          [K in keyof Usecase["actions"]]: Usecase["actions"][K] extends ActionCreatorWithoutPayload<any>
+                              ? {
+                                    sliceName: Key;
+                                    actionName: K;
+                                }
+                              : {
+                                    sliceName: Key;
+                                    actionName: K;
+                                    payload: Usecase["actions"][K] extends ActionCreatorWithPayload<
+                                        infer U
+                                    >
+                                        ? U
+                                        : never;
+                                };
+                      }[keyof {
+                          [K in keyof Usecase["actions"]]: Usecase["actions"][K] extends ActionCreatorWithoutPayload<any>
+                              ? {
+                                    sliceName: Key;
+                                    actionName: K;
+                                }
+                              : {
+                                    sliceName: Key;
+                                    actionName: K;
+                                    payload: Usecase["actions"][K] extends ActionCreatorWithPayload<
+                                        infer U
+                                    >
+                                        ? U
+                                        : never;
+                                };
+                      }]
+                    : never;
+            }]
+        >;
         middlewareEvtAction: Middleware;
     } {
         const actionTypes = new Set(
@@ -61,30 +148,33 @@ export function createMiddlewareEvtActionFactory<
         const evtAction = Evt.create<{
             sliceName: string;
             actionName: string;
+            payload: any;
         }>();
 
-        const middlewareEvtAction: Middleware = () => next => (action: { type: string }) => {
-            if (!actionTypes.has(action.type)) {
-                console.warn(
-                    [
-                        `Unknown action type ${action.type}.`,
-                        `${symToStr({ middlewareEvtAction })} is misconfigured`,
-                    ].join(" "),
-                );
-                return next(action);
-            }
+        const middlewareEvtAction: Middleware =
+            () => next => (action: { type: string; payload: any }) => {
+                if (!actionTypes.has(action.type)) {
+                    console.warn(
+                        [
+                            `Unknown action type ${action.type}.`,
+                            `${symToStr({ middlewareEvtAction })} is misconfigured`,
+                        ].join(" "),
+                    );
+                    return next(action);
+                }
 
-            const [sliceName, ...rest] = action.type.split("/");
+                const [sliceName, ...rest] = action.type.split("/");
 
-            const out = next(action);
+                const out = next(action);
 
-            evtAction.post({
-                "sliceName": sliceName,
-                "actionName": rest.join("/"),
-            });
+                evtAction.post({
+                    "sliceName": sliceName,
+                    "actionName": rest.join("/"),
+                    "payload": action.payload,
+                });
 
-            return out;
-        };
+                return out;
+            };
 
         return { "evtAction": evtAction as any, middlewareEvtAction };
     }
