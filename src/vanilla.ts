dec1623e03b69edc63065ef4a3c8d2efcf9e4c67
garronej <joseph.garrone.gj@gmail.com>
2022-10-23 23:43:08 +0200

    Add vanilla utils to use on the backend

---
+++ src/vanilla.ts
@@ -0,0 +1,42 @@
+import type { UsecasesApi, UsecaseLike } from "./createUsecasesApi";
+import type { CoreLike as CoreLike_functions } from "./usecasesToFunctions";
+import type { CoreLike as CoreLike_evts } from "./usecasesToEvts";
+import { Evt } from "evt";
+
+type CoreLike<Usecase extends UsecaseLike> = CoreLike_functions<Usecase> & CoreLike_evts;
+
+/** To use on the backend */
+export async function createVanillaApi<
+    CoreParams extends Record<string, unknown>,
+    Usecase extends UsecaseLike,
+    Core extends CoreLike<Usecase>,
+>(params: {
+    createCore: (params: CoreParams) => Promise<Core>;
+    usecasesApi: UsecasesApi<Usecase>;
+    coreParams: CoreParams;
+}) {
+    const { createCore, usecasesApi, coreParams } = params;
+
+    const { selectors, getMemoizedCoreEvts, getMemoizedCoreFunctions } = usecasesApi;
+
+    const core = await createCore(coreParams);
+
+    const {
+        getState,
+        thunksExtraArgument: { evtAction },
+    } = core;
+
+    const functions = getMemoizedCoreFunctions(core);
+
+    const coreEvts = getMemoizedCoreEvts(core);
+
+    const evtStateUpdated = Evt.asNonPostable(evtAction.pipe(() => [undefined as void]));
+
+    return {
+        getState,
+        evtStateUpdated,
+        selectors,
+        coreEvts,
+        ...functions,
+    };
+}
