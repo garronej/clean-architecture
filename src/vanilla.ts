a37a8469be66ce9d03f5d716e2b2f510673d9dce
garronej <joseph.garrone.gj@gmail.com>
2022-10-24 06:14:35 +0200

    Improve Vanilla API typing

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -1,20 +1,39 @@
-import type { UsecasesApi, UsecaseLike } from "./createUsecasesApi";
-import type { CoreLike as CoreLike_functions } from "./usecasesToFunctions";
-import type { CoreLike as CoreLike_evts } from "./usecasesToEvts";
+import type { UsecasesApi, UsecaseLike as UsecaseLike_create } from "./createUsecasesApi";
+import type { GetMemoizedCoreFunctions } from "./usecasesToFunctions";
+import type { UsecaseLike as UseCaseLike_reducer } from "./usecasesToReducer";
+import type { GenericCore } from "./createCore";
+import type { UsecaseLike as UsecaseLike_evt } from "./middlewareEvtAction";
+import type { GetMemoizedCoreEvts } from "./usecasesToEvts";
+import type { GenericSelectors } from "./usecasesToSelectors";
 import { Evt } from "evt";
+import type { NonPostableEvt } from "evt";
 
-type CoreLike<Usecase extends UsecaseLike> = CoreLike_functions<Usecase> & CoreLike_evts;
+type UsecaseLike = UsecaseLike_create & UseCaseLike_reducer & UsecaseLike_evt;
+
+type VanillaApi<
+    Usecase extends UsecaseLike,
+    ThunksExtraArgumentWithoutEvtAction extends Record<string, unknown>
+> = {
+    getState: GenericCore<ThunksExtraArgumentWithoutEvtAction, Usecase>["getState"];
+    evtStateUpdated: NonPostableEvt<void>;
+    selectors: GenericSelectors<Usecase>;
+    coreEvts: ReturnType<GetMemoizedCoreEvts<Usecase>>;
+    extras: GenericCore<ThunksExtraArgumentWithoutEvtAction, Usecase>["thunksExtraArgument"];
+    functions: ReturnType<GetMemoizedCoreFunctions<Usecase>>;
+};
 
 /** To use on the backend */
 export async function createVanillaApi<
     CoreParams extends Record<string, unknown>,
     Usecase extends UsecaseLike,
-    Core extends CoreLike<Usecase>
+    ThunksExtraArgumentWithoutEvtAction extends Record<string, unknown>
 >(params: {
-    createCore: (params: CoreParams) => Promise<Core>;
+    createCore: (
+        params: CoreParams
+    ) => Promise<GenericCore<ThunksExtraArgumentWithoutEvtAction, Usecase>>;
     usecasesApi: UsecasesApi<Usecase>;
     coreParams: CoreParams;
-}) {
+}): Promise<VanillaApi<Usecase, ThunksExtraArgumentWithoutEvtAction>> {
     const { createCore, usecasesApi, coreParams } = params;
 
     const { selectors, getMemoizedCoreEvts, getMemoizedCoreFunctions } = usecasesApi;
@@ -23,7 +42,7 @@ export async function createVanillaApi<
 
     const { getState, thunksExtraArgument: extras } = core;
 
-    const functions = getMemoizedCoreFunctions(core);
+    const functions = getMemoizedCoreFunctions(core as any);
 
     const coreEvts = getMemoizedCoreEvts(core);
 
@@ -34,7 +53,7 @@ export async function createVanillaApi<
         evtStateUpdated,
         selectors,
         coreEvts,
-        extras,
-        ...functions
+        functions,
+        extras
     };
 }
