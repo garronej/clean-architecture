27b68f48ed8fc90daa148b85134928bdd744451c
garronej <joseph.garrone@data.gouv.fr>
2022-10-26 07:09:42 +0200

    Enable to initialize the slice without private thunk

--- src/usecaseContext.ts
+++ src/usecaseContext.ts
@@ -2,13 +2,34 @@ import { assert } from "tsafe/assert";
 
 type ThunksExtraArgumentLike = { evtAction: unknown };
 
-export function createUsecaseContext<SliceContext extends Record<string, unknown>>() {
+type UsecaseContextApi<SliceContext extends Record<string, unknown>> = {
+    getUsecaseContext: (extraArg: ThunksExtraArgumentLike) => SliceContext;
+    setUsecaseContext: (extraArg: ThunksExtraArgumentLike, sliceContext: SliceContext) => void;
+};
+
+export function createUsecaseContext<
+    SliceContext extends Record<string, unknown>
+>(): UsecaseContextApi<SliceContext>;
+export function createUsecaseContext<SliceContext extends Record<string, unknown>>(
+    getInitialSliceContext: () => SliceContext
+): Omit<UsecaseContextApi<SliceContext>, "setSliceContext">;
+export function createUsecaseContext<SliceContext extends Record<string, unknown>>(
+    getInitialSliceContext?: () => SliceContext
+): UsecaseContextApi<SliceContext> {
     const weakMap = new WeakMap<ThunksExtraArgumentLike, () => SliceContext>();
 
     function getUsecaseContext(extraArg: ThunksExtraArgumentLike): SliceContext {
-        const getMemoizedSliceContext = weakMap.get(extraArg);
+        let getMemoizedSliceContext = weakMap.get(extraArg);
 
-        assert(getMemoizedSliceContext !== undefined, "Slice context not initialized");
+        if (getMemoizedSliceContext !== undefined) {
+            return getMemoizedSliceContext();
+        }
+
+        assert(getInitialSliceContext !== undefined, "Slice context not initialized");
+
+        getMemoizedSliceContext = memoize(getInitialSliceContext);
+
+        weakMap.set(extraArg, getMemoizedSliceContext);
 
         return getMemoizedSliceContext();
     }
@@ -18,21 +39,23 @@ export function createUsecaseContext<SliceContext extends Record<string, unknown
         /** If it's a function it's evaluated only on first call */
         sliceContext: SliceContext | (() => SliceContext)
     ): void {
-        const getSliceContextMemoized = (() => {
-            let cachedSlice: SliceContext | undefined = undefined;
-
-            return () => {
-                if (cachedSlice !== undefined) {
-                    return cachedSlice;
-                }
-
-                return (cachedSlice =
-                    typeof sliceContext === "function" ? sliceContext() : sliceContext);
-            };
-        })();
-
-        weakMap.set(extraArg, getSliceContextMemoized);
+        weakMap.set(
+            extraArg,
+            memoize(typeof sliceContext === "function" ? sliceContext : () => sliceContext)
+        );
     }
 
     return { getUsecaseContext, setUsecaseContext };
+}
+
+function memoize<R extends Record<string, unknown>>(fn: () => R): () => R {
+    let cache: R | undefined = undefined;
+
+    return () => {
+        if (cache !== undefined) {
+            return cache;
+        }
+
+        return (cache = fn());
+    };
 }
