f2cf3fff0444d22e23497c16e4aeab7cc80c8f55
garronej <joseph.garrone@protonmail.com>
2025-09-21 23:56:03 +0200

    Go back to an implementation with less magic

--- src/react.tsx
+++ src/react.tsx
@@ -1,12 +1,9 @@
 import type { NonPostableEvt } from "evt";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { capitalize } from "tsafe/capitalize";
 import { Reflect } from "tsafe/Reflect";
 import { Deferred } from "evt/tools/Deferred";
-import { createForwardingProxy } from "./tools/createForwardingProxy";
 import { useEvt } from "evt/hooks/useEvt";
-import { arrPartition } from "evt/tools/reducers/partition";
-import { assert, is } from "tsafe/assert";
 
 type StatesToHook<States extends Record<string, Record<string, any>>> = <
     UsecaseName extends keyof States,
@@ -31,17 +28,6 @@ type ReactApi<Core extends CoreLike, ParamsOfBootstrapCore> = {
     triggerCoreBootstrap: (params: ParamsOfBootstrapCore) => void;
 };
 
-const fpGetCoreSync = createForwardingProxy<ReactApi<CoreLike, any>["getCoreSync"]>({
-    isFunction: true
-});
-const fpGetCore = createForwardingProxy<ReactApi<CoreLike, any>["getCore"]>({ isFunction: true });
-const fpTriggerCoreBootstrap = createForwardingProxy<ReactApi<CoreLike, any>["triggerCoreBootstrap"]>({
-    isFunction: true
-});
-
-const ctxsOfUseCoreState = new WeakSet<object>();
-let core_previous: CoreLike | undefined = undefined;
-
 export function createReactApi<Core extends CoreLike, ParamsOfBootstrapCore>(params: {
     bootstrapCore: (params: ParamsOfBootstrapCore) => Promise<{ core: Core }>;
 }): ReactApi<Core, ParamsOfBootstrapCore> {
@@ -63,71 +49,39 @@ export function createReactApi<Core extends CoreLike, ParamsOfBootstrapCore>(par
     }
 
     function triggerCoreBootstrap(params: ParamsOfBootstrapCore) {
-        bootstrapCore(params).then(({ core: core_scope }) => {
-            core = core_scope;
-            transfer_handlers: {
-                if (core_previous === undefined) {
-                    break transfer_handlers;
-                }
-
-                const [handlers_internal, handlers_external] = arrPartition(
-                    core_previous.evtStateUpdated.detach(),
-                    handler => handler.ctx !== undefined && ctxsOfUseCoreState.has(handler.ctx)
-                );
-
-                if (handlers_external.length !== 0) {
-                    console.warn([
-                        `clean-architecture: HMR warning, since you have attached event`,
-                        `handlers to core.evtStateUpdated you might experience bugs due to`,
-                        `the core having been hot replaced.`
-                    ]);
-                }
-
-                handlers_internal.forEach(({ ctx, callback }) => {
-                    assert(ctx !== undefined, "clean-architecture: 3902238");
-                    assert(callback !== undefined, "clean-architecture: 39322922");
-                    assert(is<() => void>(callback));
-
-                    console.log("clean-architecture: HMR handler transferred!");
-
-                    callback();
-
-                    core_scope.evtStateUpdated.attach(ctx, callback);
-                });
-            }
-            core_previous = core_scope;
-            dCore.resolve(core_scope);
+        bootstrapCore(params).then(r => {
+            core = r.core;
+            dCore.resolve(core);
         });
     }
 
-    fpGetCoreSync.updateTarget(getCoreSync);
-    fpGetCore.updateTarget(getCore);
-    fpTriggerCoreBootstrap.updateTarget(triggerCoreBootstrap);
+    function useCoreState(usecaseName: string, selectorName: string) {
+        const core = getCoreSync();
+
+        const getSelectedState = useCallback(
+            () => core.states[usecaseName][`get${capitalize(selectorName)}`](),
+            [usecaseName, selectorName]
+        );
+
+        const [, reRenderIfSelectedStateChanged] = useState<any>(() => getSelectedState());
+
+        useEvt(
+            ctx => {
+                core.evtStateUpdated.attach(ctx, () =>
+                    reRenderIfSelectedStateChanged(getSelectedState())
+                );
+            },
+            [getSelectedState]
+        );
+
+        return getSelectedState();
+    }
 
     return {
         ofTypeCore: Reflect<Core>(),
-        getCoreSync: fpGetCoreSync.proxy as any,
-        getCore: fpGetCore.proxy as any,
-        triggerCoreBootstrap: fpTriggerCoreBootstrap.proxy,
+        getCoreSync,
+        getCore,
+        triggerCoreBootstrap,
         useCoreState: useCoreState as any
     };
-}
-
-function useCoreState(usecaseName: string, selectorName: string) {
-    const getSelectedState = () =>
-        fpGetCoreSync.proxy().states[usecaseName][`get${capitalize(selectorName)}`]();
-
-    const [, reRenderIfStateChanged] = useState<any>(() => getSelectedState());
-
-    const core = fpGetCoreSync.proxy();
-
-    useEvt(ctx => {
-        ctxsOfUseCoreState.add(ctx);
-
-        core.evtStateUpdated.attach(ctx, () => {
-            reRenderIfStateChanged(getSelectedState());
-        });
-    }, []);
-
-    return getSelectedState();
 }
